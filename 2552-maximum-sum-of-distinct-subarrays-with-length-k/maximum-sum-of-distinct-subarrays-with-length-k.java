75016b762f5a2c1d5951be1d81d3bfec73c349a7
JHANANISHRI-B <jhananishri214@gmail.com>
2026-09-12 21:33:37 +0530

    Time: 56 ms (40.53%) | Memory: 99.4 MB (20.72%) - LeetSync

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+    int n=nums.length;
+    long sum=0;
+    long max=0;
+    HashMap<Integer,Integer> map=new HashMap<>();
+
+    for(int i=0;i<k;i++){
+        sum+=nums[i];
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+    }
+
+    if(map.size()==k){
+        max=sum;
+    }
+
+    for(int i=k;i<n;i++){
+        sum-=nums[i-k];
+        map.put(nums[i-k],map.get(nums[i-k])-1);
+
+        if(map.get(nums[i-k])==0){
+            map.remove(nums[i-k]);
+        }
+
+        sum+=nums[i];
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
+        if(map.size()==k){
+            if(sum>max){
+                max=sum;
+            }
+        }
+    }
+
+    return max;
+    }
+}
